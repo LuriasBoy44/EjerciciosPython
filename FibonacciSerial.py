20a60db34d1ea5e17e8277e48c8250556510c34c
Claudio Ruiz <47365982+LuriasBoy44@users.noreply.github.com>
2021-12-06 08:39:57 -0300

    Create FibonacciSerial.py

---
+++ FibonacciSerial.py
@@ -0,0 +1,31 @@
+class Fibonacci():
+    def __init__(self):
+        self.__sec1,self.__sec2= 0,1
+
+    def ejecutaSecuencia(self,n):
+        while self.__sec1 < n:
+           # print(f'\t{a}',end='\n')
+            print('\t',self.__sec1 , self.points(self.__sec1), end='\n')        
+            self.sumaValores()
+            self.__sec1,self.__sec2 = self.__sec2, (self.__sec1+self.__sec2)
+
+
+    def sumaValores(self):
+        res=0
+        if self.__sec1>9:
+             valor=str(self.__sec1) 
+             for i in  range (len(valor)):
+                   res=res+int(valor[i])
+        else:
+           res=self.__sec1
+                   
+        print('\t>>>>',res)
+        
+    def points(self,n):  
+        txt=''
+        if n>0:
+            x = txt.rjust(n, '.')
+            return x
+        else:
+            return ''
+
